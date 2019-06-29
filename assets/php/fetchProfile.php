e8463b60dc6cfb6dc3c2bfe09ac69e607e14d556
Roninaks Ventures Pvt. Ltd <40421354+techRoninaks@users.noreply.github.com>
2019-06-29 21:19:13 +0530

    news feed dynamic edits

--- assets/php/fetchProfile.php
+++ assets/php/fetchProfile.php
@@ -6,7 +6,7 @@
 	$result = $con->query($sql);
 	if($result){
         $row = $result->fetch_array();
-        $data = array("userId"=>$row[0],"userName"=>$row[1],"userMail"=>$row[2],"userPhone"=>$row[3],"userJob"=>$row[4],"userLoc"=>$row[5],"userRating"=>$row[7]);
+        $data = array("userId"=>$row[0],"userName"=>$row["user_name"],"userMail"=>$row["user_email"],"userPhone"=>$row["user_phone"],"userJob"=>$row["user_occupation"],"userLoc"=>$row["user_location"],"ehRating"=>$row["eh_rating"]);
         $jsonData = json_encode($data);
         echo $jsonData;
 	}
